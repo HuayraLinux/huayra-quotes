9a9b54ee86ae620a3072234754e9ef4a9661ac43
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-05-29 16:50:10 -0300

    Comenzando a implementar navegador de historial con estados.

--- app/components/hq-navigation.js
+++ app/components/hq-navigation.js
@@ -2,5 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
-  classNames: ['navigation-container']
+  classNames: ['navigation-container'],
+  canBack: false,
+  canForward: false,
+
+  updateStates: function() {
+    console.log("update!");
+  }.on('didInsertElement'),
+  
 });
